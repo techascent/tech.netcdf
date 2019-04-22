f432bd2d88c3ce5f9cc7a2b1e3286de131f452b1
Harold <hhausman@gmail.com>
2019-04-22 09:48:08 -0600

    [test] Hint in test output that you need to run get data script first.

--- test/tech/netcdf_test.clj
+++ test/tech/netcdf_test.clj
@@ -7,16 +7,20 @@
 
 (deftest test-netcdf-functionality
   (resource/stack-resource-context
-   (let [test-file (netcdf/fname->netcdf netcdf/test-nc-file)
-         overview (netcdf/overview test-file)]
-     (is (= 12 (count overview)))
-     (is (= #{"tas" "ua" "area" "lon" "time" "lat" "time_bnds" "msk_rgn"
-	      "lat_bnds" "lon_bnds" "plev" "pr"}
-            (set (keys overview))))
+   (try
+     (let [test-file (netcdf/fname->netcdf netcdf/test-nc-file)
+           overview (netcdf/overview test-file)]
+       (is (= 12 (count overview)))
+       (is (= #{"tas" "ua" "area" "lon" "time" "lat" "time_bnds" "msk_rgn"
+                "lat_bnds" "lon_bnds" "plev" "pr"}
+              (set (keys overview))))
 
-     (let [fdata @(get-in overview ["tas" :data])]
-       (is (= [[215 215 215] [217 217 216] [218 218 218]]
-              (-> (tens/select fdata 0 (range 3) (range 3))
-                  ;; set the type to something we can test against
-                  (tens/clone :datatype :int32)
-                  (tens/->jvm))))))))
+       (let [fdata @(get-in overview ["tas" :data])]
+         (is (= [[215 215 215] [217 217 216] [218 218 218]]
+                (-> (tens/select fdata 0 (range 3) (range 3))
+                    ;; set the type to something we can test against
+                    (tens/clone :datatype :int32)
+                    (tens/->jvm))))))
+     (catch java.io.FileNotFoundException e
+       (println "File not found, maybe try: ./scripts/get_test_data.sh")
+       (throw e)))))
