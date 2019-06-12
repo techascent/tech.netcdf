20259192d59c8b1d3f4d269496ec5bbf1bc6918c
Chris Nuernberger <chris@techascent.com>
2019-06-12 10:48:50 -0600

    0.11

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.netcdf "0.11-SNAPSHOT"
+(defproject techascent/tech.netcdf "0.11"
   :description "netcdf bindings for tech ecosystem."
   :url "http://github.com/techascent/tech.netcdf"
   :license {:name "EPL-2.0"
@@ -6,4 +6,5 @@
   :plugins [[lein-tools-deps "0.4.5"]]
   :middleware [lein-tools-deps.plugin/resolve-dependencies-with-deps-edn]
   :lein-tools-deps/config {:config-files [:install :user :project]}
-  :repositories {"boundlessgeo" "https://repo.boundlessgeo.com/artifactory/ucar-cache"})
+  ;; :repositories {"boundlessgeo" "https://repo.boundlessgeo.com/artifactory/ucar-cache"}
+  )
