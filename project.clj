878ad8bdbc0716fa91c2fee1624c6fd54dec458b
Chris Nuernberger <chris@techascent.com>
2019-11-14 06:46:34 -0700

    0.69

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject techascent/tech.netcdf "0.68"
+(defproject techascent/tech.netcdf "0.69"
   :description "netcdf bindings for tech ecosystem."
   :url "http://github.com/techascent/tech.netcdf"
   :license {:name "EPL-2.0"
